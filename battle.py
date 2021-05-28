464da7108f710f0d5664ebe66ca9254bfe0fc798
zimnushka <g741980@gmail.com>
2021-05-28 16:15:35 +0300

    error correction

--- battle.py
+++ battle.py
@@ -1,6 +1,8 @@
 import os
 clear = lambda: os.system('cls')
 def main():
+	global healthTWO
+	global healthONE
 	healthONE=int(3)
 	healthTWO=int(3)
 	playerONEfield={}
@@ -33,6 +35,7 @@ def main():
 			player-=1
 			print(player)
 			playerbattle[str(symbol[0])][int(symbol[1])-1]=2
+			return player
 		else:
 			print("miss")
 	def print_game():
@@ -41,35 +44,33 @@ def main():
 		print("B|" + str(playerONEbattle['B']) + "      " + str(playerTWObattle['B']))
 		print("C|" + str(playerONEbattle['C']) + "      " + str(playerTWObattle['C']))
 	def game():
+		global healthTWO
+		global healthONE
 		i=int(0)
 		while i < 9:
 			print("первый игрок")
-			destroy_ship(playerTWOfield,playerTWObattle,healthTWO)
+			healthTWO = destroy_ship(playerTWOfield,playerTWObattle,healthTWO)
 			print_game()
 			if healthTWO ==0:
 				i=10
 				clear()
 				print("1 player WON")
 			print("второй игрок")
-			destroy_ship(playerONEfield,playerONEbattle,healthONE)
+			healthONE = destroy_ship(playerONEfield,playerONEbattle,healthONE)
 			print_game()
 			if healthONE ==0:
 				i=10
 				clear()
 				print("2 player WON")
-	gamers=int(input("введи количество играков"))
-	if gamers==1:
-		print(" bot")
-	else:
-		print("1 игрок раставь корабли")
-		print_field(playerONEfield)		
-		set_ship(playerONEfield)
-		clear()
-		print("2 игрок раставь корабли")
-		input("нажми на любую клавишу для продолжения")
-		print_field(playerTWOfield)		
-		set_ship(playerTWOfield)
-		clear()
-		print_game()
-		game()
+	print("1 игрок раставь корабли")
+	print_field(playerONEfield)		
+	set_ship(playerONEfield)
+	clear()
+	print("2 игрок раставь корабли")
+	input("нажми на любую клавишу для продолжения")
+	print_field(playerTWOfield)		
+	set_ship(playerTWOfield)
+	clear()
+	print_game()
+	game()
 main()
